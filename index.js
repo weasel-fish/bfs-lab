73654c65093b8e5e450cfb75c3ef86f16fef41b1
github username <github email address>
2021-09-28 11:50:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 function bfs(rootNode, vertices, edges){
+    let queue = []
+    let visited = []
+    rootNode.distance = 0
+    queue.push(rootNode)
 
+    while(queue.length > 0) {
+        let workingNode = queue.shift()
+        visited.push(workingNode)
+        let adjacentNodes = findAdjacent(workingNode.name, vertices, edges)
+        markDistanceAndPredecessor(workingNode, adjacentNodes)
+    
+        for(const node of adjacentNodes) {
+            queue.push(node)
+        }
+    }
+
+    return visited
+}
+
+function findAdjacent(node, vertices, edges) {
+    let adjacentNodes = []
+
+    for(const edge of edges) {
+        if(edge[0] == node) {
+            let newNode = vertices.find(node => node.name == edge[1])
+            if(newNode.distance == null) {
+                adjacentNodes.push(newNode)
+            }
+        } else if (edge[1] == node) {
+            let newNode = vertices.find(node => node.name == edge[0])
+            if(newNode.distance == null) {
+                adjacentNodes.push(newNode)
+            }
+        }
+    }
+    return adjacentNodes
+}
+
+function markDistanceAndPredecessor(node, adjacentNodes) {
+    for(const adjNode of adjacentNodes) {
+        adjNode.predecessor = node
+        adjNode.distance = node.distance + 1
+    }
+
+    return adjacentNodes
 }
